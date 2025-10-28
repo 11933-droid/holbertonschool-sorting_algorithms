382dec0359f9475ab915718c45fd9380683b0ad4
Yasir-AL-Shabanah <Yaser.al.shabanah@gmail.com>
2025-10-28 11:19:32 +0000

    Fix: remove unused helper; clean Bubble sort to pass -Werror

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,36 +1,37 @@
-/**
-	* bubble_sort - sort array ascending using Bubble sort
-	* @array: pointer to int array
-	* @size: number of elements
-	*
-	* Description: prints the array after each swap.
-	*/
 #include "sort.h"
-static void swap_ints(int *a, int *b)
-{
-	int t = *a;
-	*a = *b;
-	*b = t;
-}
+
+/**
+ * bubble_sort - sort an array of integers in ascending order (Bubble sort)
+ * @array: pointer to int array
+ * @size: number of elements in @array
+ *
+ * Description:
+ * - If array is NULL or size < 2, do nothing.
+ * - Print the array after each swap.
+ */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, n;
 	int swapped;
+
 	if (!array || size < 2)
-	return;
-	for (n = size; n > 1; n--)
-	{
-	swapped = 0;
-	for (i = 1; i < n; i++)
-	{
-	    if (array[i - 1] > array[i])
-	    {
-		swap_ints(&array[i - 1], &array[i]);
-		print_array(array, size);
-		swapped = 1;
-	    }
-	}
-	if (!swapped)
-	    break;
-	}
+		return;
+
+	n = size;
+	do {
+		swapped = 0;
+		for (i = 0; i + 1 < n; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				int tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
+				swapped = 1;
+			}
+		}
+		if (n > 0)
+			n--;
+	} while (swapped);
 }
--- sort.h
+++ sort.h
@@ -1,12 +1,14 @@
 #ifndef SORT_H
 #define SORT_H
+
 #include <stddef.h>
+
 /**
-	* struct listint_s - Doubly linked list node
-	* @n: Integer stored in the node
-	* @prev: Pointer to the previous element of the list
-	* @next: Pointer to the next element of the list
-	*/
+ * struct listint_s - Doubly linked list node
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
 typedef struct listint_s
 {
 	const int n;
@@ -14,14 +16,12 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* Provided by checker */
+/* Provided helpers (compiled with your code) */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-/* Sort prototypes */
+/* Mandatory tasks */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
 
 #endif /* SORT_H */
