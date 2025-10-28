a9c3862becd75831ea196b80d6784e823618edb2
root <root@ip-10-42-93-90.ec2.internal>
2025-10-28 12:51:36 +0000

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -25,6 +25,7 @@ void bubble_sort(int *array, size_t size)
 			if (array[i] > array[i + 1])
 			{
 				int tmp = array[i];
+
 				array[i] = array[i + 1];
 				array[i + 1] = tmp;
 				print_array(array, size);
